c34ec1c46c20001dd998020ad7e83070811753fa
ConsumrBuzzy <robert.d@consumrbuzz.com>
2026-03-29 17:46:02 -0400

    feat: generate Riverpod provider for Roster state management

---
+++ lib/src/providers/roster_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'roster_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$rosterHash() => r'900183b7729372db0283fa0a642f942990b388df';
+
+/// See also [Roster].
+@ProviderFor(Roster)
+final rosterProvider =
+    AutoDisposeAsyncNotifierProvider<Roster, List<SlimeView>>.internal(
+      Roster.new,
+      name: r'rosterProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$rosterHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$Roster = AutoDisposeAsyncNotifier<List<SlimeView>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
